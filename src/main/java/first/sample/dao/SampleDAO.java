a1ee0eb67772cf5f5fef995fe0e33ad8f062e7c3
ljh050777 <joongho-ya@daum.net>
2016-09-27 14:58:53 +0900

    UPDATE 20160927 14:58

--- src/main/java/first/sample/dao/SampleDAO.java
+++ src/main/java/first/sample/dao/SampleDAO.java
@@ -12,7 +12,6 @@ public class SampleDAO extends AbstractDAO{
 	
 	@SuppressWarnings("unchecked")
 	public List<Map<String, Object>> selectMainList(Map<String, Object> map) throws Exception{
-		log.debug("sampleDAO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         return (List<Map<String, Object>>)selectList("sample.selectMainList", map);
     
 	}
